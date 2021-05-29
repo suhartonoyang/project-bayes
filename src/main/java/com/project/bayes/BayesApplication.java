b547864fa1e0c214639aea1c2565c7b0a933979c
Suhartono <suhartonoyang@gmail.com>
2021-05-29 17:00:45 +0700

    Update BayesApplication.java

--- src/main/java/com/project/bayes/BayesApplication.java
+++ src/main/java/com/project/bayes/BayesApplication.java
@@ -2,8 +2,13 @@ package com.project.bayes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
 @SpringBootApplication
+@EnableSwagger2
 public class BayesApplication {
 
 	public static void main(String[] args) {
